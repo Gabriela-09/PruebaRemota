c42484e08a59357f21b0d5b3bc32db8e01110294
Gabriela-09 <33049153+Gabriela-09@users.noreply.github.com>
2017-10-24 16:15:07 -0600

    cambios

---
+++ src/pruebacongithub/Persona.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebacongithub;
+
+
+public class Persona {
+
+    public Persona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //vhvgv
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
+    private String nombre;
+    private String cedula;
+    
+}
